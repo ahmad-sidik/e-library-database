2cba1e41a8e06baded7cd697275618cac2c5df2a
ahmad-sidik <contact.ahmadsidik@gmail.com>
2024-01-01 20:35:59 +0700

    Update cuyyy

---
+++ ddl-tables.sql
@@ -0,0 +1,78 @@
+CREATE TABLE libraries (
+    library_id INT NOT NULL UNIQUE PRIMARY KEY,
+    library_name VARCHAR(255) NOT NULL UNIQUE,
+    library_location VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE books (
+    book_id INT NOT NULL UNIQUE PRIMARY KEY,
+    title VARCHAR(500) NOT NULL,
+    author VARCHAR(500) NOT NULL,
+    publisher VARCHAR(500) NOT NULL,
+    publish_date DATE,
+    ISBN VARCHAR(13)
+);
+
+CREATE TABLE users (
+    user_id INT NOT NULL UNIQUE PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE book_categories (
+    book_id INT NOT NULL UNIQUE PRIMARY KEY,
+    categories TEXT[],
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+
+CREATE TABLE book_availability (
+    book_availability_id INT NOT NULL UNIQUE PRIMARY KEY,
+    library_id INT NOT NULL,
+    book_id INT NOT NULL,
+    copies INT DEFAULT 0,
+    CONSTRAINT fk_library_id FOREIGN KEY (library_id) REFERENCES libraries(library_id),
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+
+CREATE TABLE loans (
+    loan_id INT NOT NULL UNIQUE PRIMARY KEY,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    book_availability_id INT NOT NULL,
+    total_copies INT NOT NULL CHECK(total_copies >= 1 AND total_copies <= 2),
+    loan_date TIMESTAMP NOT NULL,
+    due_date TIMESTAMP NOT NULL,
+    return_date TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id),
+    CONSTRAINT fk_book_availability_id FOREIGN KEY (book_availability_id) REFERENCES book_availability(book_availability_id)
+);
+
+CREATE TABLE holds (
+    hold_id INT NOT NULL UNIQUE PRIMARY KEY,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    book_availability_id INT NOT NULL,
+    total_copies INT NOT NULL CHECK(total_copies >= 1 AND total_copies <= 2),
+    hold_date TIMESTAMP NOT NULL,
+    exp_date TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id),
+    CONSTRAINT fk_book_availability_id FOREIGN KEY (book_availability_id) REFERENCES book_availability(book_availability_id)
+);
+
+CREATE TABLE rating (
+    rating_id INT NOT NULL UNIQUE PRIMARY KEY,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    book_availability_id INT NOT NULL,
+    rating_date TIMESTAMP NOT NULL,
+    rating INT DEFAULT 0,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books(book_id),
+    CONSTRAINT fk_book_availability_id FOREIGN KEY (book_availability_id) REFERENCES book_availability(book_availability_id)
+);
+
+
---
+++ queries.sql
@@ -0,0 +1,18 @@
+SELECT "categories", COUNT(*) AS "n_books"
+FROM (
+	SELECT UNNEST("categories") AS "categories" FROM "book_categories"
+    ) AS "unnested_categories"
+GROUP BY "categories"
+ORDER BY "n_books" desc;
+
+DROP TABLE rating;
+DROP TABLE holds;
+DROP TABLE loans;
+DROP TABLE book_categories;
+DROP TABLE book_availability;
+DROP TABLE users;
+DROP TABLE books;
+DROP TABLE libraries;
+-- buku yang favorit itu based on rating or loans
+-- buku yang jadi incaran atau antriannya banyak
+-- 
